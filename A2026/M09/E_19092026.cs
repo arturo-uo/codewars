a26e1455c5079cefe901b9a27575c34fb23c3280
Arturo Urosa <arturo_uo@hotmail.com>
2026-09-19 08:39:58 -0600

    Odd or Even

---
+++ A2026/M09/E_19092026.cs
@@ -0,0 +1,31 @@
+// Given a list of integers, determine whether the sum of its elements is odd or even.
+
+// Give your answer as a string matching "odd" or "even".
+
+// If the input array is empty consider it as: [0] (array with a zero).
+
+// Examples:
+// Input: [0]
+// Output: "even"
+
+// Input: [0, 1, 4]
+// Output: "odd"
+
+// Input: [0, -1, -5]
+// Output: "even"
+public partial class Ejercicios
+{  public string OddOrEven(int[] array)
+  {
+    int sum = 0;
+    if(array.Length <= 0)
+      return "even";
+    for(var i = 0; i < array.Length; i++)
+    {
+      sum += array[i];
+    }
+    if(sum % 2 == 0)
+      return "even";
+    else
+      return "odd";
+  }
+}
--- Program.cs
+++ Program.cs
@@ -1,3 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Ejercicios ejercicios = new Ejercicios();
-Console.WriteLine(ejercicios.findNb(2304422822859502500).ToString()); // Output: "camel Casing"	
+Console.WriteLine(ejercicios.OddOrEven(new int[] {1023, 2, 3}));
